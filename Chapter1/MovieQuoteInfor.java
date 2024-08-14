a055b6a70e4ade4b27b3f912a1976ac1baa7c75e
CDB1201 <149350781+CDB1201@users.noreply.github.com>
2024-08-14 20:37:07 +0800

    Create MovieQuoteInfor.java

---
+++ Chapter1/MovieQuoteInfor.java
@@ -0,0 +1,8 @@
+class MovieQuoteInfo {
+   public static void main(String[] args){
+     System.out.println("Movie Quote: \"Mahalin mo siya kahit sa mga araw na mahirap siyang intidihin. Lalo sa mga araw na mahirap siyang mahalin\"");
+     System.out.println("Movie: Rewind");
+     System.out.println("Character: Marian Rivera and Dingdong Dantes");
+     System.out.println("Year of the Movie: 2023");
+   }
+}
