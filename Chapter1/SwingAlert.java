34103334ef3f08b6e7c5a9130f8c3b3277673076
Carl David Binghay <149350781+CarlDavidBinghay@users.noreply.github.com>
2024-08-21 17:26:49 +0800

    Add files via upload

---
+++ Chapter1/SwingAlert.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+    public static void main(String[] args) {
+                JOptionPane.showMessageDialog(null, "Hello! I'm Carl David L. Binghay from BSIT - 2A");
+    }
+}
