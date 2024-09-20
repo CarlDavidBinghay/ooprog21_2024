319088e4ce30cfbb1741bdcc40305f5c57fb6539
Carl David Binghay <149350781+CarlDavidBinghay@users.noreply.github.com>
2024-09-20 20:22:18 +0800

    Update FirstAddressExercise.java

--- Chapter 3/FirstAddressExercise.java
+++ Chapter 3/FirstAddressExercise.java
@@ -12,3 +12,10 @@ public class FirstAddressExercise {
         System.out.println("Company: Apple Inc.");
     }
 }
+
+
+// EXPLAIN ANSWER //
+The main method is used to execute the program and the displayAddress() 
+method is used to print out the address. The System.out.println("First Java application"); 
+line is just a comment that prints out a message to the console. 
+Removing it will not affect the program's functionality.
